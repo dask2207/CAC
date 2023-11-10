f0d1cd6fa0ac2a8249f71be9c3e576eb363be235
krishna das <dask2207@gmail.com>
2023-11-10 12:24:38 +0530

    Connection with mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,55 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Approch
+
+// import express from "express"
+// const app = express()
+
+// //semicolon for cleaning (Agar previous line mai semi colon na laye toh problem na ho)
+// ;( async () => {
+//     try{
+//         mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("ERROR", error);
+//             throw err
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     }catch(error){
+//         console.error("ERROR ", error);
+//         throw err
+//     }
+// })()   
