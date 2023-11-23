d2f317bc93311987bb206b269302aeb6a7cb362c
krishna das <dask2207@gmail.com>
2023-11-23 11:23:15 +0530

    utlis code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
-        console.log(connectionInstance);
+        // console.log(connectionInstance);
         
     } catch (error) {
         console.log("MONGODB connection failed", error);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+    app.on((error) => {
+        console.log("ERROR", err);
+        throw err;
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 
